3f3e45d81c7a7465959081ee137e3fdfcc5a7bf2
2015301020092 <31877130+2015301020092@users.noreply.github.com>
2017-09-21 16:56:50 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+print("name:tangzhiyuan")
+print("##### #####  #   #")
+print("  #      #    # # ")
+print("  #     #      #  ")
+print("  #    #       #  ")
+print("  #   #####    #  ")
