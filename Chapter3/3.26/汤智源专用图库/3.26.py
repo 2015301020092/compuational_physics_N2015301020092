c352457d3ef3b6a39bc61955320b5ce45840e57d
2015301020092 <31877130+2015301020092@users.noreply.github.com>
2017-11-03 19:27:02 +0800

    Add files via upload

---
+++ Chapter3/3.26/汤智源专用图库/3.26.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 21:41:22 2017
+
+@author: 高多奇
+"""
+import pylab as pl
+import math   
+pl.figure(figsize=(7,7)) 
+
+r=[20,30,40]
+for i in range(len(r)):
+    sigma=10
+    b=8.0/3.0
+    dt=0.0001
+    x=1.
+    y=0.
+    z=0.
+    t=0.
+    X=[0]
+    Y=[0]
+    while(t<=50):
+        a_x=sigma*(y-x)
+        a_y=-x*z+r[i]*x-y
+        a_z=x*y-b*z
+        x=x+a_x*dt
+        y=y+a_y*dt
+        z=z+a_z*dt
+        t=t+dt
+       
+        X.append(t)
+        Y.append(z)
+    pl.plot(X, Y,'-',label='z versus t r='+str(r[i]))
+    pl.title('t versus z')
+    pl.ylabel('z')
+    pl.xlabel('t')
+    pl.xlim(0,30)
+    pl.ylim(0,40)
+    pl.legend(loc = 'best')
+
+
+    
